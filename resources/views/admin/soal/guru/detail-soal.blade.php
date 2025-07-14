637abaeb92ab3b35efd13867a9cb8c8249b17f97
andikspp <andhika2003.ap31@gmail.com>
2025-07-14 19:57:27 +0700

    fixing route di edit soal dan hapus soal

--- resources/views/admin/soal/guru/detail-soal.blade.php
+++ resources/views/admin/soal/guru/detail-soal.blade.php
@@ -193,8 +193,8 @@
                             html += `<td>${q.kompetensi ? q.kompetensi.nama : '-'}</td>
                         <td>${q.indikator ? q.indikator.nama : '-'}</td>
                         <td>
-                            <a href="/admin/soal/edit/guru/${q.id}" class="btn btn-warning btn-sm">Edit</a>
-                            <form id="delete-form-${q.id}" action="/admin/soal/${q.id}" method="POST" style="display:inline;">
+                             <a href="{{ route('admin.soal.edit.guru', '') }}/${q.id}" class="btn btn-warning btn-sm">Edit</a>
+                            <form id="delete-form-${q.id}" action="{{ route('hapus.soal', '') }}/${q.id}" method="POST" style="display:inline;">
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                 <input type="hidden" name="_method" value="DELETE">
                                 <button type="button" class="btn btn-danger btn-sm" onclick="confirmDelete(${q.id})">Hapus</button>
